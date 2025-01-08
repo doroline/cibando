4b1435bf74f0ee34058a050ec4f2eaf35b347d80
Danilo-jdk <danilodefilio.jdk@gmail.com>
2025-01-08 15:32:45 +0100

    fine seconda lezione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
-  percorso = "../assets/images/carousel-";
-  images = [
-    {id: 1, label: 'Spaghetti al pomodoro'},
-    {id: 2, label: 'Tagliata di manzo'},
-    {id: 3, label: 'Tiramisù classico'}
-  ];
-
 
   allievi = ['Davide', 'Francesco', 'Maria Sole', 'Gioele'];
   allieviCompleta = [
