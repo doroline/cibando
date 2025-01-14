40118fd0b4989dfa580d7e346835f43a539219b4
Danilo-jdk <danilodefilio.jdk@gmail.com>
2025-01-14 15:30:09 +0100

    fine sesta lezione

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -11,13 +11,29 @@ export class RegistrationComponent {
   form = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){6,15}$/)]),
     ripetiPassword: new FormControl('', [Validators.required]),
     accetto: new FormControl(false, [Validators.requiredTrue])
   })
+
+  passwordConvalidata = false;
 
   onSubmit(){
     console.log(this.form.value)
   }
 
+  controlloPassword(e){
+    if(e === this.form.controls.password.value){
+      this.passwordConvalidata = true;
+    } else {
+      this.passwordConvalidata = false;
+    }
+  }
+convalidaForm(): boolean{
+  if(this.form.valid && this.passwordConvalidata){
+    return false
+  } else {
+    return true
+  }
+}
 }
